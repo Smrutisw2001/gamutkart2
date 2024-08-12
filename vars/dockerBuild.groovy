8c7d6cb23bf1c7e5eb5a83de4fa60b4518ea9312
Smrutisw2001 <147130047+Smrutisw2001@users.noreply.github.com>
2024-08-12 20:54:07 +0530

    Delete vars/dockerBuild.groovy

--- vars/dockerBuild.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-  script{
-                    withDockerRegistry(credentialsId: 'Docker'){
-                        sh"docker build -t smrutisw2001/gamutkart:123 ."
-                        sh"docker push smrutisw2001/gamutkart:123"
-                    }
-                }
-}
